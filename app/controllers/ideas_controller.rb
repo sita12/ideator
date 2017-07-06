dfd33ae80ed5e46005245a45030eec3cecfb4c2f
sita12 <sitaneupane5700@gmail.com>
2017-07-06 21:12:18 +0545

    Adding Flash Messages

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,29 +2,37 @@ class IdeasController < ApplicationController
 	def index
     @ideas = Idea.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
   end
-  	def create 
-		@idea = Idea.create(idea_params)
-		redirect_to root_path
-	end
+  	  def create    
+        @idea = Idea.create(idea_params)
+        if @idea.valid?
+         flash[:success] = "Your idea has been posted!"
+        else
+         flash[:alert] = "Woops! Looks like there has been an error!"
+        end
+       redirect_to root_path
+      end
 
 	def edit 
 		@idea = Idea.find(params[:id])
 	end
 
-	def update
-  		@idea = Idea.find(params[:id])
-  		if @idea.update(idea_params)
-    		redirect_to root_path
-  		else
-    		redirect_to edit_idea_path(params[:id])
-  		end
-	end
+  def update
+    @idea = Idea.find(params[:id])
+    if @idea.update(idea_params)
+      flash[:success] = "The idea has been updated!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Woops! Looks like there has been an error!"
+      redirect_to edit_idea_path(params[:id])
+    end
+  end
 
-	def destroy
-  		@idea = Idea.find(params[:id])
-  		@idea.destroy
-  		redirect_to root_path
-	end
+def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    flash[:success] = "The idea was successfully deleted!"
+    redirect_to root_path
+end
 	
 	private 
 
